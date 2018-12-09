5605d543017a2ff9efaf33670dd40a2ebc6448fb
GuillaumeLiard <liardg@gmail.com>
2018-12-09 20:21:50 +0100

    remove page style

--- pages/index.vue
+++ pages/index.vue
@@ -15,28 +15,3 @@
 		}
 	}
 </script>
-
-<style>
-.page {
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-
-}
-.title {
-	height: 10%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-.main {
-	width: 100%;
-	height: 60%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-
-</style>
